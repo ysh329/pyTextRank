c509e55ce49649118a1307e569407109d3d87407
yuens <ysh329@sina.com>
2016-05-06 19:24:13 +0800

    Initialize

---
+++ pyTextRank/TextRank4Sentence.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+################################### PART0 DESCRIPTION #################################
+# Filename: TextRank4Sentence.py
+# Description:
+#
+
+
+# Author: Shuai Yuan
+# E-mail: ysh329@sina.com
+# Create:　2016-05-06 09:07:46
+# Last:
+__author__ = 'yuens'
+
+################################### PART1 IMPORT ######################################
+import jieba
+
+################################### PART2 CLASS && FUNCTION ###########################
+
+for i in list(jieba.cut("我今天看到他，发现他心情不好")): print i
+
+import mxnet
+
+print mxnet
